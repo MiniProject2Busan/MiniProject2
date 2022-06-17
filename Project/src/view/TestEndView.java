2386fe66ee69d03a1210955772f957d7f74aea46
parkhanju <parkhanju95@gmail.com>
2022-06-17 13:37:30 +0900

    test update

--- Project/src/view/TestEndView.java
+++ Project/src/view/TestEndView.java
@@ -1,11 +1,16 @@
 package view;
 
 import dto.InstructorDTO;
+import dto.ManagerDTO;
 
-public class TestInstructorEndView {
+public class TestEndView {
 	
 	public static void instructorVeiw(InstructorDTO instructor) {
 		System.out.println(instructor);
 	}
-
+	
+	public static void managerView(ManagerDTO manager) {
+		System.out.println(manager);
+	}
+ 
 }
