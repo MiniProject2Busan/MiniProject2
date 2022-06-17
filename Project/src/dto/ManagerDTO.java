0023601f2b996556c4a3db83b27ca904abda07cf
parkhanju <parkhanju95@gmail.com>
2022-06-17 13:37:44 +0900

    manager update

--- Project/src/dto/ManagerDTO.java
+++ Project/src/dto/ManagerDTO.java
@@ -1,5 +1,39 @@
 package dto;
 
 public class ManagerDTO {
-
+	private int manager_id; // 담당자 id
+	private String manager_name; // 담당자 이름
+	private String manager_phone; // 담당자 연락처
+	
+	public ManagerDTO() {}
+	public ManagerDTO(int manager_id, String manager_name, String manager_phone) {
+		super();
+		this.manager_id = manager_id;
+		this.manager_name = manager_name;
+		this.manager_phone = manager_phone;
+	}
+	public int getManagerId() {
+		return manager_id;
+	}
+	public void setManagerId(int manager_id) {
+		this.manager_id = manager_id;
+	}
+	public String getManagerName() {
+		return manager_name;
+	}
+	public void setManagerName(String manager_name) {
+		this.manager_name = manager_name;
+	}
+	public String getManagerPhone() {
+		return manager_phone;
+	}
+	public void setManagerPhone(String manager_phone) {
+		this.manager_phone = manager_phone;
+	}
+	@Override
+	public String toString() {
+		return "ManagerDTO [manager_id=" + manager_id + ", manager_name=" + manager_name + ", manager_phone="
+				+ manager_phone + "]";
+	}
+	
 }
